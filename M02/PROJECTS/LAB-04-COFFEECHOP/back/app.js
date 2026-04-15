252db8d21a4abebb560e0957b0ec0b71b77b0acc
RDK <photocromatic@gmail.com>
2026-04-15 13:30:07 -0600

    ws

--- M02/PROJECTS/LAB-04-COFFEECHOP/back/app.js
+++ M02/PROJECTS/LAB-04-COFFEECHOP/back/app.js
@@ -1,8 +1,22 @@
+const dotenv = require( 'dotenv' );
 const express = require( 'express' );
+const mongoose = require( 'mongoose' );
 
 const postsRouter = require( './src/routers/posts.router' );
 const app = express();
 const port = 8282;
+
+dotenv.config();
+
+mongoose
+    .connect( process.env.MONGO_URI )
+    .then( () =>  {
+        console.log( "Conexion extablecida" );
+    })
+    .catch( ( e ) => {
+        console.log( "No se pudo establecer la Conexion", e );
+    })
+
 
 app.use( postsRouter );
 
