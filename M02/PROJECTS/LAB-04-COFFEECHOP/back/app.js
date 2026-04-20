1e9352c1f2feeec2b5f32d14bbefbb59a5c2ff7e
RDK <photocromatic@gmail.com>
2026-04-20 16:27:14 -0600

    ws

--- M02/PROJECTS/LAB-04-COFFEECHOP/back/app.js
+++ M02/PROJECTS/LAB-04-COFFEECHOP/back/app.js
@@ -1,21 +1,29 @@
+// IMPORT EXTERNAL/BUILT-IN MODULES
+const dns = require( 'node:dns' );
 const dotenv = require( 'dotenv' );
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 const bodyParser = require( 'body-parser' );
 
+// IMPORT CUSTOM MODULES
 const postsRouter = require( './src/routers/posts.router' );
+
+// SETUP MODULES
+dns.setServers( [ '8.8.8.8', '8.8.4.4' ] );
+dotenv.config();
+
+// SETUP APP
 const app = express();
 const port = 8282;
 
-dotenv.config();
-
+// CONNECT DATABASE
 mongoose
     .connect( process.env.MONGO_URI )
     .then( () =>  {
-        console.log( "Conexion extablecida" );
+        console.log( "Success Connection..!" );
     })
     .catch( ( e ) => {
-        console.log( "No se pudo establecer la Conexion", e );
+        console.log( "Failure Connection...", e );
     })
 
 app.use( bodyParser.json() );
