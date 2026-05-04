c03e285112597f508c79412bd0212daf7fdc4433
RDK <photocromatic@gmail.com>
2026-05-04 13:38:17 -0600

    ws

--- M02/PROJECTS/LAB-04-COFFEECHOP/front/src/components/CardProduct/CardProduct.jsx
+++ M02/PROJECTS/LAB-04-COFFEECHOP/front/src/components/CardProduct/CardProduct.jsx
@@ -1,9 +1,11 @@
 import styles from './CardProduct.module.css';
 
-function CardProduct() {
+function CardProduct( { name, category, description } ) {
     return (
         <div className={ styles.cardProduct }>
-
+            <h3>{ name }</h3>
+            <h5>{ category }</h5>
+            <blockquote>{ description }</blockquote>
         </div>
     );
 }
--- M02/PROJECTS/LAB-04-COFFEECHOP/front/src/pages/Products/Products.jsx
+++ M02/PROJECTS/LAB-04-COFFEECHOP/front/src/pages/Products/Products.jsx
@@ -1,27 +1,28 @@
 import CardProduct from '../../components/CardProduct/CardProduct';
 import styles from './Products.module.css';
 
+const products = [
+    {
+        "id": 1,
+        "name": "Café Latte",
+        "category": "beverages",
+        "description": "Suave espresso con leche vaporizada y una pequeña capa de espuma. Perfecto para comenzar el día."
+    },
+    {
+        "id": 2,
+        "name": "Pastel de Chocolate",
+        "category": "foods",
+        "description": "Delicioso bizcocho de chocolate oscuro con cobertura cremosa y chispas de cacao."
+    },
+    {
+        "id": 3,
+        "name": "Jugo Verde",
+        "category": "beverages",
+        "description": "Mezcla refrescante de espinaca, manzana verde, apio, pepino y un toque de limón."
+    },
+];
+
 function Products() {
-    const products = [
-        {
-            "id": 1,
-            "name": "Café Latte",
-            "category": "beverages",
-            "description": "Suave espresso con leche vaporizada y una pequeña capa de espuma. Perfecto para comenzar el día."
-        },
-        {
-            "id": 2,
-            "name": "Pastel de Chocolate",
-            "category": "foods",
-            "description": "Delicioso bizcocho de chocolate oscuro con cobertura cremosa y chispas de cacao."
-        },
-        {
-            "id": 3,
-            "name": "Jugo Verde",
-            "category": "beverages",
-            "description": "Mezcla refrescante de espinaca, manzana verde, apio, pepino y un toque de limón."
-        },
-    ];
 
     return (
         <div className={ styles.products }>
@@ -32,7 +33,8 @@ function Products() {
                     <CardProduct
                         key={ product.id }
                         name={ product.name }
-                        precio={ product.precio }
+                        category={ product.category }
+                        description={ product.description }
                     />
                 ))}
             </div>
