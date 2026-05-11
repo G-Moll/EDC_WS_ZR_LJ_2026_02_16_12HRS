73fc56e15b0708e9ccadf4d59239d243f345eeac
RDK <photocromatic@gmail.com>
2026-05-11 13:58:41 -0600

    ws

--- M02/PROJECTS/LAB-04-COFFEECHOP/back/src/controllers/posts.controller.js
+++ M02/PROJECTS/LAB-04-COFFEECHOP/back/src/controllers/posts.controller.js
@@ -56,4 +56,61 @@ exports.createPost = async ( request, response ) => {
     }
 }
 
+exports.updatePost = async ( request, response ) => {
+    try {
+        const { _id } = request.body;
 
+        const post = await Post.findByIdAndUpdate(
+            _id,
+            request.body, 
+            {
+                returnDocument: 'after',
+                runValidators: true
+            }
+        );
+        
+        if( !post ) {
+            return response
+                .status( 404 )
+                .json( { error: 'Post no encontrado' } );
+        }
+        
+        console.log( `Post ${ _id } actualizado:`, request.body );
+        response
+            .status( 200 )
+            .json( { post: post } );
+    }
+    catch( e ) {
+        response
+            .status( 400 )
+            .json( { error: e.message } );
+    }
+}
+
+exports.deletePost = async ( request, response ) => {
+    try {
+        const postId = request.params.id;
+        const deletedPost = await Post.findOneAndDelete( { _id: postId } );
+        
+        if( ! deletedPost ) {
+            return response
+                .status( 404 )
+                .json({ 
+                    error: 'Post no encontrado',
+                    message: `No existe ningún post con el ID: ${ postId }`
+                });
+        }
+        
+        response
+            .status( 200 )
+            .json({ 
+                message: 'Post eliminado correctamente',
+                deletedPost: deletedPost
+            });
+    }
+    catch( e ) {
+        response
+            .status( 400 )
+            .json( { error: e.message } );
+    }
+}
--- M02/PROJECTS/LAB-04-COFFEECHOP/back/src/routers/posts.router.js
+++ M02/PROJECTS/LAB-04-COFFEECHOP/back/src/routers/posts.router.js
@@ -6,6 +6,8 @@ const postsRouter = express.Router();
 postsRouter.get( "/posts", postsController.getPosts );
 postsRouter.get( "/posts/:id", postsController.getPost );
 postsRouter.post( "/posts", postsController.createPost );
+postsRouter.put( "/posts", postsController.updatePost );
+postsRouter.delete( "/posts/:id", postsController.deletePost );
 
 module.exports = postsRouter;
 
